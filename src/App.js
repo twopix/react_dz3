fdffd78c3c6c44a3ce4900a2bc4cab5878ec5e5e
George Melidis <melidis@gmail.com>
2017-11-18 16:31:01 +0200

    53 passed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,125 @@
-import React, {Component} from 'react';
-import './App.css';
-import Step from './Step';
+import React, { Component } from "react";
+import "./App.css";
+import Step from "./Step";
+import PersonalForm from "./PersonalForm";
+import CardForm from "./CardForm";
 
-const stepTitles = ['Personal information', 'Card information', 'Finish'];
+const stepTitles = ["Personal information", "Card information", "Finish"];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 1,
+      firstName: "",
+      lastName: "",
+      email: "",
+      cardNumber: "",
+      isTimeOver: false
+    };
+    this.handleTabClick = this.handleTabClick.bind(this);
+    this.handleChangeForm = this.handleChangeForm.bind(this);
+    this.handleClickNextForm = this.handleClickNextForm.bind(this);
+    this.handleChangeTimeOver = this.handleChangeTimeOver.bind(this);
+    this.isFormCommitable = this.isFormCommitable.bind(this);
+    this.renderForm = this.renderForm.bind(this);
+  } //constructor
+
+  handleTabClick(number) {
+    this.setState({
+      step: number
+    });
+  } //handleTabClick
+
+  handleChangeForm(name, value) {
+    this.setState({ [name]: value });
+  } //handleChangeForm
+
+  handleClickNextForm() {
+    if (this.state.step === 3) return;
+    let thisStep = this.state.step;
+    this.setState({
+      step: ++thisStep
+    });
+  } //handleClickNextForm
+
+  handleChangeTimeOver(bool) {
+    let timeOverStatus = this.state.isTimeOver;
+    if (bool) this.setState({ isTimeOver: !timeOverStatus });
+  } //handleChangeTimeOver
+
+  isFormCommitable() {
+    switch (this.state.step) {
+      case 1:
+        return (
+          this.state.firstName !== "" &&
+          this.state.lastName !== "" &&
+          this.state.email !== "" &&
+          this.state.email.includes("@")
+        );
+      case 2:
+        return this.state.cardNumber.length === 16;
+      default:
+        return false;
+    }
+  } //isFormCommitable
+
+  renderForm() {
+    switch (this.state.step) {
+      case 1:
+        return (
+          <PersonalForm
+            firstName={this.state.firstName}
+            lastName={this.state.lastName}
+            email={this.state.email}
+            onChangeForm={this.handleChangeForm}
+          />
+        );
+      case 2:
+        return (
+          <CardForm
+            cardNumber={this.state.cardNumber}
+            onChangeForm={this.handleChangeForm}
+            onChangeTimeOver={this.handleChangeTimeOver}
+          />
+        );
+      case 3:
+        return "Поздравляем!";
+      default:
+        return false;
+    }
+  } //renderForm
+
   render() {
     return (
-      <div className="container" >
+      <div>
+        <div className="container">
           <div className="tab-panel">
+            {stepTitles.map((title, index) => (
+              <Step
+                key={title}
+                number={index + 1}
+                onClick={this.handleTabClick}
+                isSelected={this.state.step - 1 === index}
+                isClickable={
+                  index !== this.state.step - 1 && index < this.state.step
+                }
+              >
+                {title}
+              </Step>
+            ))}
           </div>
-          <div className="form-content">
-          </div>
+          <div className="form-content">{this.renderForm()}</div>
           <div className="button-panel">
-            <button className="button-panel button-next" />
+            <button
+              disabled={!this.isFormCommitable() || this.state.isTimeOver}
+              className="button-next"
+              onClick={this.handleClickNextForm}
+            >
+              Next
+            </button>
           </div>
+        </div>
       </div>
     );
   }
--- src/CardForm.js
+++ src/CardForm.js
@@ -1,21 +1,21 @@
-import React, {Component} from 'react';
-import Title from './Title';
-import './CardForm.css';
+import React, { Component } from "react";
+import Title from "./Title";
+import "./CardForm.css";
 
 export class CardForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      leftTime: 120
-    };
+    this.state = { leftTime: 120 };
     props.onChangeTimeOver(false);
+
+    this.handleChangeForm = this.handleChangeForm.bind(this);
   }
 
   componentDidMount() {
     this.id = setInterval(() => {
       const leftTime = Math.max(this.state.leftTime - 1, 0);
-      this.setState({leftTime});
+      this.setState({ leftTime });
       if (leftTime === 0 && this.state.leftTime === 1) {
         this.props.onChangeTimeOver(true);
       }
@@ -26,11 +26,18 @@ export class CardForm extends Component {
     clearInterval(this.id);
   }
 
+  handleChangeForm(event) {
+    const { onChangeForm } = this.props;
+    const { name, value } = event.target;
+    onChangeForm(name, value);
+  } //handleChangeForm
+
   render() {
-    const {leftTime} = this.state;
+    const { leftTime } = this.state;
     return (
-      <div>
+      <div className="card-form">
         <Title>Номер карты</Title>
+        <input type="text" name="cardNumber" onChange={this.handleChangeForm} />
         <p className="left-time">Осталось {leftTime} секунд</p>
       </div>
     );
